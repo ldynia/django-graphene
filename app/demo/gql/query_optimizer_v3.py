6158a139682ec03cc9db427be43b5da4d0d90d70
ldynia <change@me.com>
2021-08-05 17:01:24 +0200

    adding prefix nodes

--- app/demo/gql/query_optimizer_v3.py
+++ app/demo/gql/query_optimizer_v3.py
@@ -1,27 +1,11 @@
 from collections import Counter
 
-from graphql.language.ast import FieldNode
 from django.db.models.fields.related import ForeignKey
 from django.db.models.fields.related import ManyToManyField
 from django.db.models.fields.reverse_related import ManyToManyRel
 from django.db.models.fields.reverse_related import ManyToOneRel
 from django.db.models.fields.reverse_related import OneToOneRel
 
-"""
-query AllCities {
-  allCities {
-    state {
-      name
-      country {
-        name
-        continent {
-          name
-        }
-      }
-    }
-  }
-}
-"""
 
 """
 query AllCities {
@@ -56,6 +40,22 @@ query AllCities {
   }
 }
 """
+"""
+query {
+  allLibrary {
+    results {
+      book {
+        authors {
+          fullName
+        }
+        owner {
+          firstName
+        }
+      }
+    }
+  }
+}
+"""
 
 def optimizer(qs, info, top_node=None):
     relations = []
@@ -67,14 +67,14 @@ def optimizer(qs, info, top_node=None):
     if not top_node:
         top_node = info.field_nodes[0]
 
-
-    for field in top_node.selection_set.selections:
-        field_name = field.name.value
+    prefixes = []
+    for field_node in top_node.selection_set.selections:
+        field_name = field_node.name.value
         # Check if filed exists on model
         if hasattr(qs.model, field_name):
             models_field = qs.model._meta.get_field(field_name)
             # Check if field has nested models
-            if field.selection_set:
+            if field_node.selection_set:
                 # Check if field is in select related relationship
                 if isinstance(models_field, (ForeignKey, ManyToManyField, OneToOneRel)):
                     relations_prefixes['select_related'].append(field_name)
@@ -83,22 +83,26 @@ def optimizer(qs, info, top_node=None):
                 if isinstance(models_field, (ManyToOneRel, ManyToManyRel)):
                     relations_prefixes['prefetch_related'].append(field_name)
 
-                # Append prefix to relations
-                prefix = extract_path(field, field_name)
-                relations.append(prefix)
+                # Prefix map
+                prefixes.append((field_name, field_node))
 
+    # Append prefix to relations
+    relations = extract_path(prefixes)
+
+    print('Before Normalization', relations)
     # Normalize paths in relations
     relations = normalize_paths(relations)
+    print('After Normalization', relations)
 
     # Extract select_related and prefetch_related paths
     select_related, prefetch_related = get_related(relations, relations_prefixes)
     print('select_related', select_related)
     print('prefetch_related', prefetch_related)
 
-    return qs.select_related(*select_related).prefetch_related(*prefetch_related)
+    return qs.select_related(*select_related).prefetch_related(*prefetch_related)[:10]
 
 
-def extract_path(field_node: FieldNode, prefix='') -> str:
+def extract_path(prefixes=[], res=set()) -> str:
     """
     Recursively extract model name from from graphql query.
 
@@ -107,12 +111,43 @@ def extract_path(field_node: FieldNode, prefix='') -> str:
 
     :return: str: relations between models
     """
-    if field_node.selection_set:
-        for sub_node in field_node.selection_set.selections:
-            if sub_node.selection_set:
-                prefix = prefix + '__' + sub_node.name.value
-                return extract_path(sub_node, prefix)
-    return prefix
+    sub_prefix = []
+    for prx in prefixes:
+        path, field_node = prx
+        # Field node has nested models
+        if field_node and field_node.selection_set:
+            for sub_node in field_node.selection_set.selections:
+                if sub_node.selection_set:
+                    path = path + '__' + sub_node.name.value
+                    sub_prefix.append([path, sub_node])
+                else:
+                    sub_prefix.append([path, None])
+        # Field node doesn't have nested models
+        else:
+            res.add(path)
+
+    # Make recursive call only if there is a data
+    if sub_prefix:
+        res = remove_duplicates(sub_prefix, res)
+        return extract_path(sub_prefix, res)
+
+    return res
+
+    # prefix_subnode = []
+    # if field_node.selection_set:
+    #     for sub_node in field_node.selection_set.selections:
+    #         if sub_node.selection_set:
+    #             lp = field_node.name.value + '__' + sub_node.name.value
+    #             for prx in prefix:
+    #                 local_prefixes.append(prx + '__' + sub_node.name.value)
+    #     return extract_path(sub_node, prefix)
+    # return prefix
+
+
+def remove_duplicates(data: list, results: set) -> set:
+    for item in data:
+        results.discard(item[0])
+    return results
 
 
 def get_related(relations: list, relation_prefixes: dict) -> tuple:
@@ -152,7 +187,8 @@ def normalize_paths(paths: list) -> list:
     :return: list: Normalized paths.
     """
     normalized_paths = []
-    for path in paths:
+
+    for path in set(paths):
         models = path.split('__')
         if len(models) == 1:
             normalized_paths.append(path)
